a134fed85266a4a19804e95b2ae4f24edad2e8cc
Jon Klein <jonklein2021@gmail.com>
2026-03-05 15:53:52 -0500

    nvim: add mark.nvim and update keymaps file for beauty

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -51,11 +51,11 @@ require("config.keymaps")
 
 -- Enable language servers
 vim.lsp.enable({
-    'bash-language-server',
-    'clangd',
-    'lua-language-server',
-    'omnisharp',
-    'pyright',
-    'vscode-json-language-server',
-    'vtsls'
+	'bash-language-server',
+	'clangd',
+	'lua-language-server',
+	'omnisharp',
+	'pyright',
+	'vscode-json-language-server',
+	'vtsls'
 })
