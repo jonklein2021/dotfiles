2e6fcdf86a8b9a01fa83610ba678cfae2b2416b1
Jon Klein <jonklein2021@gmail.com>
2026-03-12 12:03:49 -0400

    nvim: finally fix barbar configuration

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,23 +1,17 @@
 return {
 	"romgrk/barbar.nvim",
-	config = function()
-		require("lazy").setup({
-			{
-				dependencies = {
-					"lewis6991/gitsigns.nvim",
-					"nvim-tree/nvim-web-devicons",
-				},
-				init = function()
-					vim.g.barbar_auto_setup = false
-				end,
-				opts = {
-					sidebar_filetypes = {
-						NvimTree = true,
-						["neo-tree"] = { event = "BufWipeout" },
-					},
-				},
-				version = "^1.0.0",
-			},
-		})
+	dependencies = {
+		"lewis6991/gitsigns.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
 	end,
+	opts = {
+		sidebar_filetypes = {
+			NvimTree = true,
+			["neo-tree"] = { event = "BufWipeout" },
+		},
+	},
+	version = "^1.0.0",
 }
