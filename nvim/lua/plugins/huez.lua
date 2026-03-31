c151c1a56ecdb3805cc00491ab0e0d0d45171473
Jon Klein <jonklein2021@gmail.com>
2026-03-31 11:30:07 -0400

    nvim: add huez dynamic theme plugin

---
+++ nvim/lua/plugins/huez.lua
@@ -0,0 +1,15 @@
+return {
+    "vague2k/huez.nvim",
+    import = "huez-manager.import",
+    branch = "stable",
+    event = "UIEnter",
+    config = function()
+        require("huez").setup()
+
+        local pickers = require("huez.pickers")
+        vim.keymap.set("n", "<leader>co", pickers.themes, {})
+        vim.keymap.set("n", "<leader>cof", pickers.favorites, {})
+        vim.keymap.set("n", "<leader>col", pickers.live, {})
+        vim.keymap.set("n", "<leader>coe", pickers.ensured, {})
+    end,
+}
