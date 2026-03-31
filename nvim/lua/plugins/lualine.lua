fa9dcfe31d13039bfe9a711a3a54fc7e717a8cf2
Jon Klein <jonklein2021@gmail.com>
2026-03-31 11:46:55 -0400

    change lualine theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,8 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup({
-            options = { theme = "dracula" }
+            options = { theme = "tomorrow_night" }
         })
     end
 }
-
