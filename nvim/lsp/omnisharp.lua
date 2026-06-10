d85afff54dcef0ab5eeb8578c66c66def94d52fe
Jon Klein <jonklein2021@gmail.com>
2026-06-10 11:22:42 -0400

    little changes to dotnet version and lsp

--- nvim/lsp/omnisharp.lua
+++ nvim/lsp/omnisharp.lua
@@ -1,6 +1,6 @@
 return {
     cmd = { 'OmniSharp', '--languageserver' },
-    root_markers = { 'package.json', { '.git', '.gitignore' } },
+    root_markers = { '*.csproj', { '.git', '.gitignore' } },
     filetypes = { 'cs' },
     single_file_support = true,
     settings = {
